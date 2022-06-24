54978267fcd70adfc66f658d15880e4aa665ade3
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-06-24 20:43:16 +0530

    Time: 19 ms (74.35%), Space: 8.6 MB (64.54%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    // dp[i] = maximum sum after we make k partition of the array
-    int solve(vector<int>&arr, int k, vector<int>&dp, int cur) {
-        int n = arr.size();
-        if(cur >= arr.size()) return 0;
-        if(dp[cur] != -1) return dp[cur];
-        // at each k we can partition the arry
-        int maxNum = 0, sum = 0;
-        for(int i=cur; i<min(n, cur+k); i++) {
-            maxNum = max(maxNum, arr[i]);
-            sum = max(sum , maxNum * (i - cur + 1) + solve(arr, k, dp, i+1));
-        }
-        return dp[cur] = sum;
-    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int>dp(n+1, -1);
-        return solve(arr, k, dp, 0);
+        vector<int>dp(n+1, 0);
+        for(int i=1; i<=n; i++) {
+            int curMax = arr[i-1], best = dp[i-1];
+            for(int j=1; i-j >= 0 && j <= k; j++) {
+                curMax = max(curMax, arr[i-j]);
+                best = max(best, curMax * j + dp[i-j]);
+            }
+            dp[i] = best;
+        }
+        return dp[n];
     }
 };
