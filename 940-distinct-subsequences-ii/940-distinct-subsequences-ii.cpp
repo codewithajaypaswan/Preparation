024d97f19ffe2ca1b176b854aabca0633b361caa
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-06-27 15:15:33 +0530

    Time: 8 ms (54.74%), Space: 7.2 MB (15.69%) - LeetHub

--- 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     int distinctSubseqII(string s) {
         long n = s.size(), mod = 1e9+7, ans = 0, added = 0;
-        vector<long>endWith(26, 0);
-        for(int i=0; i<n; i++) {
-            added = (ans - endWith[s[i] - 'a'] + 1)%mod;
-            ans = (ans + added)%mod;
-            endWith[s[i] - 'a'] = (endWith[s[i] - 'a']+added)%mod;
+        vector<long>endWith(26, -1);
+        vector<long>dp(n+1);
+        dp[0] = 1;
+        for(int i=1; i<=n; i++) {
+            dp[i] = (dp[i-1]*2)%mod; // concatening the character at the end
+            if(endWith[s[i-1]-'a'] >= 0) {
+                dp[i] = (dp[i] - dp[endWith[s[i-1]-'a']]+mod)%mod;
+            }
+            endWith[s[i-1] - 'a'] = i-1;
         }
-        return (ans+mod)%mod;
+        // remove the non empty subsequcence
+        return (dp[n]-1)%mod;
     }
 };
