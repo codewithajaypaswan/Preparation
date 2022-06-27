df9f8baf3d1bbf21337d9ae2adc846935ca6ea28
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-06-27 15:08:01 +0530

    Time: 4 ms (79.58%), Space: 6.5 MB (77.61%) - LeetHub

--- 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int distinctSubseqII(string s) {
-        long n = s.size(), mod = 1e9+7, ans = 0;
-        vector<long>dp(n+1, 1);
+        long n = s.size(), mod = 1e9+7, ans = 0, added = 0;
+        vector<long>endWith(26, 0);
         for(int i=0; i<n; i++) {
-            for(int j=0; j<i; j++) {
-                if(s[i] != s[j]) {
-                    dp[i] = (dp[i] + dp[j])%mod;
-                }
-            }
-            ans = (dp[i] + ans)%mod;
+            added = (ans - endWith[s[i] - 'a'] + 1)%mod;
+            ans = (ans + added)%mod;
+            endWith[s[i] - 'a'] = (endWith[s[i] - 'a']+added)%mod;
         }
-        return ans;
+        return (ans+mod)%mod;
     }
 };
