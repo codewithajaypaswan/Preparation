961792d8012eb03c97d2278f166cfeed6ad50454
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-06-27 12:13:52 +0530

    Time: 81 ms (28.15%), Space: 14.7 MB (46.85%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -3,14 +3,13 @@ public:
     int minTaps(int n, vector<int>& ranges) {
         vector<int>dp(n+1, 1e9);
         dp[0] = 0;
-        for (int i = 0; i <= n; i++) {
-			int start = max(i - ranges[i], 0);
-			int end = min(i + ranges[i], n);
-			for (int j = start+1; j <= end; j++) {
-				dp[j] = min(dp[j], dp[start] + 1);
-			}
-		}
-
-		return dp[n] == 1e9 ? -1 : dp[n];
+        for(int i=0; i<=n; i++) {
+            int start = max(0, i+-ranges[i]);
+            int end = min(n, i+ranges[i]);
+            for(int j=start+1; j<=end; j++) {
+                dp[j] = min(dp[j], 1 + dp[start]);
+            }
+        }
+        return dp[n] == 1e9 ? -1 : dp[n];
     }
 };
