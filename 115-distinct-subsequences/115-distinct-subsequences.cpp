2bbb31b0180fa30fe4268137715861c539635f37
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-06-27 11:09:59 +0530

    Time: 60 ms (46.08%), Space: 18.5 MB (46.72%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int solve(string &s, string &t, vector<vector<int>>&dp, int i, int j) {
-        if(j == t.size()) return 1;
-        if(i == s.size()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == t[j]) {
-            return dp[i][j] = 0LL + solve(s, t, dp, i+1, j+1) + solve(s, t, dp, i+1, j);
-        }
-        else {
-            return dp[i][j] = solve(s, t, dp, i+1, j);
-        }
-    }
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-        vector<vector<int>>dp(n, vector<int>(m, -1));
-        return solve(s, t, dp, 0, 0);
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        for(int i=0; i<=n; i++) dp[i][0] = 1;
+        for(int i=1; i<=n; i++) {
+            for(int j=1;j<=m; j++) {
+                if(s[i-1] == t[j-1]) {
+                    dp[i][j] =0LL+dp[i-1][j-1] + dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
+        
     }
 };
