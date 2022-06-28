cb8db948d700e9adc0fd3dba5414aff60a5033fe
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-06-28 09:29:33 +0530

    Time: 440 ms (5.43%), Space: 124.9 MB (5.43%) - LeetHub

---
+++ 960-delete-columns-to-make-sorted-iii/960-delete-columns-to-make-sorted-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs[0].size(), ans = INT_MAX;
+        vector<int>dp(n, 0);
+        for(int i=1; i<n; i++) {
+            dp[i] = i;
+            for(int j=i-1; j>=0; j--) {
+                if(check(strs, i, j)) {
+                    dp[i] = min(dp[i], i-j-1 + dp[j]);
+                }
+            }
+        }
+        // return dp[n-1];
+        // for(int i=0; i<n; i++) cout<<dp[i]<<" ";
+        for(int i=0; i<n; i++) {
+            ans = min(ans, dp[i] + n - i - 1);
+        }
+        return ans;
+    }
+    bool check(vector<string>&str, int i, int j) {
+        for(string s:str) {
+            if(s[i] < s[j]) return false;
+        }
+        return true;
+    }
+};
