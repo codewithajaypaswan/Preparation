337eb1a06f3f237c765cff5268e0347d454b84e3
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-06-30 22:05:10 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -12,26 +12,31 @@ class Solution
 	int ans = 0;
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
-        pq.push({0, 0});
-        vector<int>vis(V, 0);
-        int ans = 0, E = V;
+        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>>pq;
+        for(int i=0; i<V; i++) {
+            for(auto u:adj[i]) {
+                int v = u[0], wt = u[1];
+                pq.push({wt, {i, v}});
+            }
+        }
+        int E = V-1;
+        vector<int>parent(V, -1);
         while(!pq.empty()) {
             auto it = pq.top(); pq.pop();
-            int u = it.second, wt = it.first;
-            if(vis[u]) continue;
-            vis[u] = 1;
+            int wt = it.first, u = it.second.first, v = it.second.second;
+            int parentU = find(u, parent);
+            int parentV = find(v, parent);
+            if(parentU == parentV) continue;
+            parent[parentU] = parentV;
             ans += wt;
             E--;
             if(E == 0) return ans;
-            for(auto x:adj[u]) {
-                int v = x[0], wt_v = x[1];
-                if(vis[v] == 0) {
-                    pq.push({wt_v, v});
-                }
-            }
         }
         return ans;
+    }
+    int find(int u, vector<int>&parent) {
+        if(parent[u] == -1) return u;
+        return parent[u] = find(parent[u], parent);
     }
 };
 
