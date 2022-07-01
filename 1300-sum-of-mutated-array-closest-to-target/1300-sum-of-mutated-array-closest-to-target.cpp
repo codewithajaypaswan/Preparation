b676c759db37711c8edd6c57bd4b0604670ded31
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-01 05:37:05 +0530

    Time: 12 ms (97.36%), Space: 12.4 MB (34.30%) - LeetHub

---
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findBestValue(vector<int>& arr, int target) {
+        int low = 0, high = target, sum = 0, mx = 0;
+        for(int x:arr) {
+            sum += x;
+            mx = max(mx, x);
+        }
+        if(sum <= target) return mx;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            int sum = solve(mid, arr);
+            if(sum == target) return mid;
+            if(sum < target) {
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        int choice1 = solve(low, arr);
+        int choice2 = solve(low-1, arr);
+        if(abs(target - choice1) < abs(target - choice2)) return low;
+        return low-1;
+    }
+    int solve(int mid, vector<int>&arr) {
+        int ans = 0;
+        for(int x:arr) {
+            ans += x > mid ? mid : x;
+        }
+        return ans;
+    }
+};
