1e106b7198974ffc0943f5dcb8383e03d5727747
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-02 00:28:56 +0530

    Time: 328 ms (44.13%), Space: 76 MB (12.49%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -5,8 +5,21 @@ public:
         for(int x:nums) sum += x;
         if(sum&1) return false;
         int n = nums.size(), target = sum/2;
-        vector<vector<int>>dp(n+1, vector<int>(sum/2+1, -1));
-        return solve(nums, dp, 0, target);
+        vector<vector<int>>dp(n+1, vector<int>(sum/2+1, 0));
+        for(int i=0; i<=n; i++) {
+            dp[i][0] = 1;
+        }
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=sum/2; j++) {
+                if(nums[i-1] <= j) {
+                    dp[i][j] = 0LL + dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum/2];
     }
     bool solve(vector<int>&nums, vector<vector<int>>&dp, int cur, int target) {
         if(target == 0) return true;
