952e011ff1c434000c3909ed0dc591032df47b98
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-05 18:18:30 +0530

    Time: 76 ms (42.42%), Space: 34.7 MB (47.91%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -4,8 +4,8 @@ public:
         if(cur < 0 || cur >= arr.size() || arr[cur] < 0) return false;
         if(arr[cur] == 0) return true;
         int jump = arr[cur];
-        arr[cur] = -1;
-        return solve(arr, cur + jump) || solve(arr, cur - jump);
+        arr[cur] *= -1;
+        return solve(arr, cur+jump) || solve(arr, cur - jump);
     }
     bool canReach(vector<int>& arr, int start) {
         return solve(arr, start);
