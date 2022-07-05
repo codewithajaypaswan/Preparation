2165d564782e516d1e75c0fc6928bf8d27788572
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-05 17:36:39 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (60.31%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    int solve(string &s, int k, vector<vector<int>>&dp, int cnt, int ind) {
-        if(ind >= s.size()) return 0;
-        if(dp[ind][cnt] != -1) return dp[ind][cnt];
-        if(s[ind] == '0') {
-            return dp[ind][cnt] = max(1 + solve(s, k, dp, cnt+1, ind+1), solve(s, k, dp, cnt, ind+1));
-        }
-        else {
-            int temp = 0;
-            if(cnt < 31 && k - (1<<cnt) >= 0) 
-                temp = max(temp, 1 + solve(s, k - (1<<cnt), dp, cnt+1, ind+1));
-            return dp[ind][cnt] = max(temp, solve(s, k, dp, cnt, ind+1));
-        }
-        return dp[ind][cnt];
-    }
     int longestSubsequence(string s, int k) {
-        int n = s.size();
-        reverse(s.begin(), s.end());
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return solve(s, k, dp, 0, 0);
+        long sum = 0, j = 0, n = s.size(), ans = 0, check = 0;
+        for(int i=n-1; i>=0; i--) {
+            if(s[i] == '0') ans++;
+            else if(check == 0 && j < 31) {
+                sum += (1<<j);
+                if(k - sum >= 0) ans++;
+            }
+            j++;
+        }
+        return ans;
     }
 };
