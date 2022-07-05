5abf19e4c26ec91654506f20fa2a44f7e8dec7d2
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-05 17:47:06 +0530

    Time: 124 ms (35.41%), Space: 48.4 MB (42.95%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    bool solve(vector<int>&nums, vector<int>&dp, int cur) {
-        if(cur >= nums.size()-1) return true;
-        if(dp[cur] != -1) return dp[cur];
-        int n = nums.size();
-        int jump = nums[cur], ans = 0;
-        for(int i=cur; i<min(n, cur + jump); i++) {
-            ans |= solve(nums, dp, i+1);
-            if(ans) return dp[cur] = ans;
-        }
-        return dp[cur] = ans;
-    }
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>dp(n, -1);
-        return solve(nums, dp, 0);
+        int maxJump = 0, n = nums.size();
+        for(int i=0; i<n; i++) {
+            if(i > maxJump) return false;
+            maxJump = max(maxJump, i + nums[i]);
+        }
+        return true;
     }
 };
