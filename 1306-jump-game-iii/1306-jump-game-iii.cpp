e855b74c753deacbb20c9b8186a7b93f8e19bc54
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-05 18:22:40 +0530

    Time: 84 ms (32.15%), Space: 31.2 MB (85.82%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    bool solve(vector<int>&arr, int cur) {
-        if(cur < 0 || cur >= arr.size() || arr[cur] < 0) return false;
-        if(arr[cur] == 0) return true;
-        int jump = arr[cur];
-        arr[cur] *= -1;
-        return solve(arr, cur+jump) || solve(arr, cur - jump);
-    }
     bool canReach(vector<int>& arr, int start) {
-        return solve(arr, start);
+        queue<int>q;
+        int n = arr.size();
+        q.push(start);
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            if(arr[cur] == 0) return true;
+            int jump = arr[cur];
+            if(cur + jump < n && arr[cur + jump] >= 0) q.push(cur + jump);
+            if(cur - jump >= 0 && arr[cur - jump] >= 0) q.push(cur - jump);
+            arr[cur] = -1;
+        }
+        return false;
     }
 };
