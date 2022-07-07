deb2415ce4e18e2460aa06b016cb74ea203c88a7
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-07 20:37:48 +0530

    Added solution - LeetHub

--- Recursively remove all adjacent duplicates - GFG/recursively-remove-all-adjacent-duplicates.cpp
+++ Recursively remove all adjacent duplicates - GFG/recursively-remove-all-adjacent-duplicates.cpp
@@ -10,25 +10,28 @@ using namespace std;
 class Solution{
 public:
     string rremove(string s){
-        int n = s.size();
-        int i = 0, last = 0;
-        string ans = "";
-        while(i < n) {
-            int j = i+1, flag = 0;
-            while(j < n && s[i] == s[j]) {
-                j++;
-                flag = 1, last = 1;
+        int last = 1;
+        while(last) {
+            last = 0;
+            string ans = "";
+            int i = 0, n = s.size();
+            while(i < n) {
+                int j = i+1, flag = 0;
+                while(j < n && s[i] == s[j]) {
+                    j++;
+                    flag = 1, last = 1;
+                }
+                if(flag) {
+                    i = j;
+                }
+                else {
+                    ans += s[i];
+                    i++;
+                }
             }
-            if(flag) {
-                i = j;
-            }
-            else {
-                ans += s[i];
-                i++;
-            }
+            s = ans;
         }
-        if(last) return rremove(ans);
-        return ans;
+        return s;
     }
 };
 
