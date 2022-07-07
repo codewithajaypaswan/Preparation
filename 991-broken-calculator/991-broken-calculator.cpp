e1eccfe3d973477147b6e3b54a2ee0fc3f1b0e2b
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-07 10:58:41 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.17%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
-        int ans = 0;
-        while(startValue < target) {
-            if(target%2 == 0) target /= 2;
-            else target += 1;
-            ans++;
-        }
-        return ans + startValue - target;
+        if(startValue >= target) return startValue - target;
+        if(target%2 == 0) return 1 + brokenCalc(startValue, target/2);
+        return 1 + brokenCalc(startValue, target + 1);
     }
 };
