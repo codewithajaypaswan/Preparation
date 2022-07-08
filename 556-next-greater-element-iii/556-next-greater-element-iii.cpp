20c053cdcbdcb0ddda766d035a3ebd86f5cbde61
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-08 15:59:57 +0530

    Time: 0 ms (100.00%), Space: 6 MB (24.61%) - LeetHub

---
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s = to_string(n);
+        int i = s.size()-2;
+        while(i >= 0 && s[i] >= s[i+1]) i--;
+        if(i >= 0) {
+            int j = s.size() - 1;
+            while(j >= 0 && s[j] <= s[i]) j--;  
+            swap(s[i], s[j]);
+        }
+        reverse(s.begin()+i+1, s.end());
+        // cout<<s<<endl;
+        long x = stoll(s);
+        return (x > INT_MAX || x <= n) ? -1 : x; 
+    }
+};
