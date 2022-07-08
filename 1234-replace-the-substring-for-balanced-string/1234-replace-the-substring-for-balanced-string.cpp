c6f11df8f67583f1a9224d66911a725d61052437
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-08 09:11:07 +0530

    Time: 57 ms (62.60%), Space: 7.8 MB (47.64%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int balancedString(string s) {
+        unordered_map<char, int>mp;
+        for(char c:s) {
+            mp[c]++;
+        }
+        int k = s.size()/4, ans = 1e9;
+        for(int high = 0, low = 0; high < s.size(); high++) {
+            mp[s[high]]--;
+            while(low <= high+1 && mp['Q'] <= k && mp['W'] <= k && mp['E'] <= k && mp['R'] <= k) {
+                ans = min(ans, high - low + 1);
+                mp[s[low++]]++;
+            }
+        }
+        return ans;
+    }
+};
