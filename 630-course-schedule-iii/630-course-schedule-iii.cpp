fe035b76ce14759cd8773ec6167e1348c19a1989
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-11 03:28:23 +0530

    Time: 2198 ms (7.77%), Space: 265 MB (13.62%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -8,18 +8,15 @@ public:
         priority_queue<int>pq;
         int time = 0;
         for(int i=0; i<courses.size(); i++) {
-            if(courses[i][0] <= courses[i][1]) {
-                if(courses[i][0] + time <= courses[i][1]) {
-                    pq.push(courses[i][0]);
-                    time += courses[i][0];
-                }
-                else if(pq.top() > courses[i][0]) {
-                    // cout<<courses[i][0]<<endl;
-                    time -= pq.top(); pq.pop();
-                    pq.push(courses[i][0]);
-                    time += courses[i][0];
-                    // cout<<time<<
-                }
+            if(courses[i][0] + time <= courses[i][1]) {
+                pq.push(courses[i][0]);
+                time += courses[i][0];
+            }
+            else if(!pq.empty() && pq.top() > courses[i][0]) {
+                time -= pq.top();
+                pq.pop();
+                pq.push(courses[i][0]);
+                time += courses[i][0];
             }
         }
         return pq.size();
