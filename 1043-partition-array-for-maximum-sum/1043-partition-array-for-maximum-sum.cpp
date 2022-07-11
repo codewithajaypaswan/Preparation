ba3c6a651cd3252b50fd768f33d928bd2dcfca31
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-11 09:45:19 +0530

    Time: 50 ms (13.29%), Space: 8.6 MB (32.26%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int dfs(vector<int>&arr, int k, vector<int>&dp, int cur) {
+    int solve(vector<int>&arr, int k, int cur, vector<int>&dp) {
         if(cur >= arr.size()) return 0;
+        int ans = 0, n = arr.size(), mx = 0;
         if(dp[cur] != -1) return dp[cur];
-        int maxNum = 0, sum = 0;
-        for(int i=cur; i<min((int)arr.size(), cur + k); i++) {
-            int len = i - cur + 1;
-            maxNum = max(maxNum, arr[i]);
-            sum = max(sum, maxNum * len + dfs(arr, k, dp, i+1));
+        for(int i=cur; i<min(n, cur+k); i++) {
+            mx = max(mx, arr[i]);
+            ans = max(ans, mx * (i - cur + 1) + solve(arr, k, i+1, dp));
         }
-        return dp[cur] = sum;
+        return dp[cur] = ans;
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
         vector<int>dp(n+1, -1);
-        return dfs(arr, k, dp, 0);
+        return solve(arr, k, 0, dp);
     }
 };
