e12d999bc0bcca207703dc401d4fc977fe2c1da1
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-11 10:04:26 +0530

    Added solution - LeetHub

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -12,9 +12,9 @@ class Solution{
     *   S: source vertex to start traversing graph with
     *   V: number of vertices
     */
-    vector <int> bellman_ford(int V, vector<vector<int>> adj, int s) {
+    vector <int> bellman_ford(int V, vector<vector<int>> adj, int S) {
         vector<int>dist(V, 1e8);
-        dist[s] = 0;
+        dist[S] = 0;
         for(int i=0; i<V-1; i++) {
             for(auto it:adj) {
                 int u = it[0], v = it[1], wt = it[2];
