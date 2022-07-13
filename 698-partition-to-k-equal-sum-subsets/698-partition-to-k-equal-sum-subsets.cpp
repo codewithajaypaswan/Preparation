8b2f399aefb06c53ad9a16d28de38fd6b5f37ff0
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-13 12:06:12 +0530

    Time: 229 ms (48.48%), Space: 21.1 MB (20.05%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     //1, first approack is backtracking
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        long sum = 0, n = nums.size();
+        int n = nums.size();
+        int sum = 0;
         for(int x:nums) sum += x;
-        if(sum%k != 0 || k == 0) return false;
-        int target = sum/k;
+        if(k == 0 || sum%k != 0) return false;
         vector<int>dp((1<<n), -1);
+        return solve(nums, dp, sum/k, k);
+    }
+    bool solve(vector<int>&nums, vector<int>&dp, int target, int k) {
+        int n = nums.size();
         dp[0] = 0;
-        for(int mask=0; mask < (1<<n); mask++) {
+        for(int mask = 0; mask < (1<<n); mask++) {
             if(dp[mask] == -1) continue;
-            for(int i=0; i<n; i++) {
-                if(!(mask & (1<<i)) && dp[mask] + nums[i] <= target) {
-                    dp[mask | (1<<i)] = (dp[mask] + nums[i])%target;
-                }
+            for(int i=0; i < n; i++) {
+                if(!(mask&(1<<i)) && dp[mask] + nums[i] <= target)
+                dp[mask|(1<<i)] = (dp[mask] + nums[i])%target;
             }
-        } 
-        // 1111
-        return dp[(1<<n) - 1] == 0;
+        }
+        return dp[(1<<n)-1] == 0;
     }
 };
