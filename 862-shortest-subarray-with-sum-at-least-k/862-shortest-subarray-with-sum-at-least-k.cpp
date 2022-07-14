5073dba8d8ad95f29c37a73285b559758401c6bd
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-15 01:17:06 +0530

    Time: 374 ms (56.82%), Space: 110 MB (39.25%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
-        int n = nums.size(), ans = INT_MAX;
-        deque<int>dq;
-        vector<long long>pre(n+1, 0);
-        for(int i=0; i<n; i++) {
-            pre[i+1] = pre[i] + nums[i];
-        }
-        for(int i=0; i<=n; i++) {
-            while(!dq.empty() && pre[i] - pre[dq.front()] >= k) {
-                ans = min(ans, i - dq.front());
+        long n = nums.size(), ans = INT_MAX;
+        long sum = 0;
+        deque<pair<long,long>>dq;
+        for(long i=0; i<n; i++) {
+            sum += nums[i];
+            if(sum >= k) {
+                ans = min(ans, i+1);
+            }
+            while(!dq.empty() && sum - dq.front().second >= k) {
+                ans = min(ans, i - dq.front().first);
                 dq.pop_front();
             }
-            while(!dq.empty() && pre[i] <= pre[dq.back()]) dq.pop_back();
-            dq.push_back(i);
+            while(!dq.empty() && sum <= dq.back().second) {
+                dq.pop_back();
+            }
+            dq.push_back({i, sum});
         }
         return ans == INT_MAX ? -1 : ans;
     }
