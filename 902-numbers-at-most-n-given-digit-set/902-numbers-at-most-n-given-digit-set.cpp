269383c4a6c9e9b92c8d131147dc60bc6965f073
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-16 17:48:58 +0530

    Time: 5 ms (27.01%), Space: 8 MB (73.95%) - LeetHub

--- 902-numbers-at-most-n-given-digit-set/902-numbers-at-most-n-given-digit-set.cpp
+++ 902-numbers-at-most-n-given-digit-set/902-numbers-at-most-n-given-digit-set.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    
-    int dp[11][2][2];
-    
-    int solve(vector<string>& digits, int pos, string num, bool limflag, bool started, string st){
-        if(pos==num.size()){
-            // cout<<st<<" ";   // you can print all the numbers that are made : blank space means : 0
-            return 1;
+    int dp[15][2][2];
+    int solve(string &s, vector<string>&digits, int ind, int bound, int started) {
+        if(ind >= s.size()) return started == 1 ? 1 : 0;
+        if(dp[ind][bound][started] != -1) return dp[ind][bound][started];
+        int maxDigit = bound ? s[ind] - '0' : 9;
+        int ans = 0;
+        if(started == 0) {
+            ans = solve(s, digits, ind+1, 0, 0);
+            for(int i=0; i<digits.size(); i++) {
+                int cur = stoi(digits[i]);
+                if(cur > maxDigit) continue;
+                ans += solve(s, digits, ind+1, bound & (cur == maxDigit), 1);
+            }
         }
-        int cnt=0;
-        if(dp[pos][limflag][started]!=-1) return dp[pos][limflag][started];
-        int lim=9;
-        if(limflag) lim=num[pos]-'0';
-        if(!started) cnt+= solve(digits,pos+1,num,false,false,st);
-        for(string d:digits){
-            int x = d[0]-'0';
-            if(x>lim) continue;
-            if(x==lim && limflag) cnt+= solve(digits,pos+1,num,true,true,st+d[0]);
-            else cnt+= solve(digits,pos+1,num,false,true,st+d[0]);
+        else {
+            for(int i=0; i<digits.size(); i++) {
+                int cur = stoi(digits[i]);
+                if(cur > maxDigit) continue;
+                ans += solve(s, digits, ind+1, bound & (cur == maxDigit), started);
+            }
         }
-        dp[pos][limflag][started] = cnt;
-        return cnt;
+        return dp[ind][bound][started] = ans;
     }
-    
     int atMostNGivenDigitSet(vector<string>& digits, int n) {
-        string num = to_string(n);
-        memset(dp,-1,sizeof dp);
-        string x;
-        return solve(digits, 0, num, true, false, x)-1; // to exclude 0
+        memset(dp, -1, sizeof(dp));
+        string s = to_string(n);
+        return solve(s, digits, 0, 1, 0);
     }
 };
