a1d2642fcb23a8b5d97dc122ac92d2df25b4b81a
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-18 03:18:53 +0530

    Time: 60 ms (57.05%), Space: 10.5 MB (64.10%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int mod = 1e9+7;
+    int solve(int n, int k) {
+        if(k == 0) return 1;
+        if(k < 0 || n <= 0) return 0;
+        if(dp[n][k] != -1) return dp[n][k];
+        int res = (solve(n-1, k) + solve(n, k-1))%mod;
+        return dp[n][k] = (res - solve(n-1, k-n) + mod)%mod;
+    }
+    int kInversePairs(int n, int k) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n, k);
+    }
+};
