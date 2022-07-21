7646c4406c6021280d2ac1409f9f33c9b124091b
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-21 17:48:32 +0530

    Time: 89 ms (33.27%), Space: 7.8 MB (48.61%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -8,7 +8,7 @@ public:
         int k = s.size()/4, ans = 1e9;
         for(int high = 0, low = 0; high < s.size(); high++) {
             mp[s[high]]--;
-            while(low <= high+1 && mp['Q'] <= k && mp['W'] <= k && mp['E'] <= k && mp['R'] <= k) {
+            while(low <= high + 1 && mp['Q'] <= k && mp['W'] <= k && mp['E'] <= k && mp['R'] <= k) {
                 ans = min(ans, high - low + 1);
                 mp[s[low++]]++;
             }
