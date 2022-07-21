3463e6eecb7c4cc860481392a2816a22dd44d951
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-21 18:13:37 +0530

    Time: 1075 ms (12.07%), Space: 152.5 MB (30.03%) - LeetHub

--- 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
+++ 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
         int n = s.size();
-        vector<vector<int>>hash(n+1, vector<int>(26, 0));
+        vector<vector<int>>v(n+1, vector<int>(26, 0));
         for(int i=1; i<=n; i++) {
-            for(int j=0; j<26; j++) hash[i][j] = hash[i-1][j];
-            hash[i][s[i-1] - 'a']++;
+            for(int j=0; j<26; j++) v[i][j] = v[i-1][j];
+            v[i][s[i-1] - 'a']++;
         }
         vector<bool>ans;
         for(auto q:queries) {
             int l = q[0], r = q[1], k = q[2];
-            int temp = 0;
+            int cnt = 0;
             for(int i=0; i<26; i++) {
-                temp += (hash[r+1][i] - hash[l][i])%2;
+                cnt += (v[r+1][i] - v[l][i])%2;
             }
-            if(temp/2 <= k) ans.push_back(true);
-            else ans.push_back(false);
+            if(cnt/2 <= k) {
+                ans.push_back(true);
+            }
+            else {
+                ans.push_back(false);
+            }
         }
         return ans;
     }
