fc995aa4222ca50c99f1787c420c0cf834f568e2
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-21 17:38:36 +0530

    Time: 6 ms (6.72%), Space: 6 MB (66.16%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -5,8 +5,8 @@ public:
         int i = s.size()-2;
         while(i >= 0 && s[i] >= s[i+1]) i--;
         if(i >= 0) {
-            int j = s.size() - 1;
-            while(j >= 0 && s[j] <= s[i]) j--;  
+            int j = s.size()-1;
+            while(j >= 0 && s[j] <= s[i]) j--;
             swap(s[i], s[j]);
         }
         reverse(s.begin()+i+1, s.end());
