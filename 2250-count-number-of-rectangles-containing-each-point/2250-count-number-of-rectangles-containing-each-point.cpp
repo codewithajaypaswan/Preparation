5bea6e462e7458fab7c42aef84df66180083419d
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-07-27 09:22:00 +0530

    Time: 1522 ms (14.04%), Space: 95.9 MB (20.46%) - LeetHub

--- 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.cpp
+++ 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.cpp
@@ -7,9 +7,9 @@ public:
             mp[r[1]].push_back(r[0]); // {height, {width1, width2, width3}} // here we can apply BS
         }
         for(int i=0; i<=100; i++) {
-            // if(mp[i].size() > 0) {
+            if(mp[i].size() > 0) {
                 sort(mp[i].begin(), mp[i].end());
-            // }
+            }
         }
         for(auto p:points) {
             int x = p[0], y = p[1], cnt = 0;
