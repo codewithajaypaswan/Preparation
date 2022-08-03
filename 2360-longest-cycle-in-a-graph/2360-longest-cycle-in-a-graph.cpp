f6c95a6f47381cceeaac00bcca71553a523f3fa9
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-08-03 10:39:37 +0530

    Time: 253 ms (89.23%), Space: 100 MB (75.02%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int ans = INT_MIN;
+    void dfs(vector<int>&edges, vector<int>&vis, int cur, vector<int>&dist, int len) {
+        if(cur == -1) return;
+        if(vis[cur] == 0) {
+            vis[cur] = 1;
+            dist[cur] = len;
+            if(edges[cur] != -1) dfs(edges, vis, edges[cur], dist, len + 1);
+            dist[cur] = -1;
+        }
+        else {
+            if(dist[cur] != -1) ans = max(ans, len - dist[cur]);
+        }
+    }
+    int longestCycle(vector<int>& edges) {
+        int n = edges.size();
+        vector<int>vis(n, 0), dist(n, -1);
+        for(int i=0; i<n; i++) {
+            if(vis[i] == 0) {
+                dfs(edges, vis, i, dist, 0);
+            }
+        }
+        return ans == INT_MIN ? -1 : ans;
+    }
+};
