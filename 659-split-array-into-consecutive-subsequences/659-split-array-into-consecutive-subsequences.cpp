40f2946195aa21963d0879a96fe3a86229b47063
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-08-20 00:23:53 +0530

    Time: 132 ms (72.02%), Space: 58.5 MB (27.74%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     bool isPossible(vector<int>& nums) {
-        unordered_map<int,int>freq, mp;
-        for(int x:nums) freq[x]++;
+        unordered_map<int,int>mp, freq;
+        for(int x:nums) {
+            freq[x]++;
+        }
         for(int x:nums) {
             if(freq[x] == 0) continue;
             else if(mp[x] > 0) {
-                mp[x]--;
                 freq[x]--;
+                mp[x]--;
                 mp[x+1]++;
             }
-            else if(freq[x+1] > 0 && freq[x+2] > 0) {
+            else if(freq[x+1] && freq[x+2]) {
                 freq[x]--;
                 freq[x+1]--;
                 freq[x+2]--;
                 mp[x+3]++;
             }
-            else return false;
+            else {
+                return false;
+            }
         }
         return true;
     }
