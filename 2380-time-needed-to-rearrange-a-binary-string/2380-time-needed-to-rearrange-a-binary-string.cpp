b4ba4865224ba0345a3abdb2aa6c97f2608e3762
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-08-23 03:59:25 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (10.00%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int secondsToRemoveOccurrences(string s) {
-        int flag = 1, ans = 0;
-        while(flag) {
-            flag = 0;
-            for(int i=0; i<s.size()-1; i++) {
-                if(s[i] == '0' && s[i+1] == '1') {
-                    swap(s[i], s[i+1]);
-                    i++;
-                    flag = 1;
-                }
+        int cnt = 0, ans = 0;
+        for(int i=0; i<s.size(); i++) {
+            cnt += s[i] == '0' ? 1 : 0;
+            if(s[i] == '1' && cnt > 0) {
+                ans = max(ans + 1, cnt);
             }
-            ans += flag;
         }
         return ans;
     }
