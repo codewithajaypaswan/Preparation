993de17f89b968b91e1f9b58009f81140d787c42
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-10-17 05:34:00 +0530

    Time: 159 ms (29.30%), Space: 7.9 MB (29.73%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int findMax(vector<int>&jobs, int ind) {
-        int ans = INT_MIN;
+        int mx = INT_MIN;
         for(int i=ind; i<jobs.size(); i++) {
-            ans = max(ans, jobs[i]);
+            mx = max(mx, jobs[i]);
         }
-        return ans;
+        return mx;
     }
     int solve(vector<int>&jobs, int d, int ind, vector<vector<int>>&dp) {
         if(ind >= jobs.size()) return 0;
         if(d == 1) return findMax(jobs, ind);
         if(dp[ind][d] != -1) return dp[ind][d];
-        int n = jobs.size() - d + 1, mn = -1e9, ans = 1e9;
-        for(int i=ind; i<n; i++) {
-            mn = max(mn, jobs[i]);
-            ans = min(ans, mn + solve(jobs, d-1, i+1, dp));
+        int mx = -1e9, ans = 1e9;
+        for(int i=ind; i<jobs.size()-d+1; i++) {
+            mx = max(mx, jobs[i]);
+            ans = min(ans, mx + solve(jobs, d-1, i+1, dp));
         }
         return dp[ind][d] = ans;
     }
-    int minDifficulty(vector<int>& jobs, int d) {
-        int n = jobs.size();
+    int minDifficulty(vector<int>& job, int d) {
+        int n = job.size();
         if(n < d) return -1;
         vector<vector<int>>dp(n+1, vector<int>(d+1, -1));
-        return solve(jobs, d, 0, dp);
+        return solve(job, d, 0, dp);
     }
 };
