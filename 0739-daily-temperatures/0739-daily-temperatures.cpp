271ec8a2a105b494384f66bc0ef7828ac851a8dd
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-10-19 10:32:45 +0530

    Time: 420 ms (14.48%), Space: 98.7 MB (5.00%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -4,11 +4,9 @@ public:
         int n = temp.size();
         vector<int>ans(n, 0);
         stack<int>st;
-        for(int i=0; i<n; i++) {
-            while(!st.empty() && temp[st.top()] < temp[i]) {
-                ans[st.top()] = i - st.top();
-                st.pop();
-            }
+        for(int i=n-1; i>=0; i--) {
+            while(!st.empty() && temp[st.top()] <= temp[i]) st.pop();
+            if(!st.empty()) ans[i] = st.top() - i;
             st.push(i);
         }
         return ans;
