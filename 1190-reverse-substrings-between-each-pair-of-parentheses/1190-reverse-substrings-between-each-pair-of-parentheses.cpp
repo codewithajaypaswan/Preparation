a91b3e6dc50a68656d7c9e3ee04d93c7ad143458
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2022-10-20 04:27:12 +0530

    Time: 7 ms (16.11%), Space: 6.3 MB (52.89%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
+        int n = s.size();
+        vector<int>pairs(n+1);
         stack<int>st;
-        string ans = "";
-        for(char c:s) {
-            if(c == '(') st.push(c);
-            else if(c == ')') {
-                string temp = "";
-                while(!st.empty() && st.top() != '(') {
-                    temp += st.top();
-                    st.pop();
-                }
-                st.pop();
-                for(char c:temp) {
-                    st.push(c);
-                }
+        for(int i=0; i<n; i++) {
+            if(s[i] == '(') st.push(i);
+            else if(s[i] == ')') {
+                int j = st.top(); st.pop();
+                pairs[j] = i;
+                pairs[i] = j;
+            }
+        }
+        string ans;
+        for(int i=0, dir = 1; i < n && i >= 0; i+=dir) {
+            if(s[i] == '(' || s[i] == ')') {
+                dir = -dir;
+                i = pairs[i];
             }
             else {
-                st.push(c);
+                ans += s[i];
             }
         }
-        while(!st.empty()) {
-            ans += st.top(); st.pop();
-        }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
