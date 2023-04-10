2d6e48d2392d4f73fbeacc0ed97ee9a7c9de0a79
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2023-04-10 07:47:05 +0530

    Time: 4 ms (39.51%), Space: 6.4 MB (56.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,13 +4,11 @@ public:
         stack<char>st;
         for(char c:s) {
             if(c == '(' || c == '{' || c == '[') st.push(c);
-            else if(!st.empty()){
-                if(st.top() == '(' && c == ')') st.pop();
-                else if(st.top() == '{' && c == '}') st.pop();
-                else if(st.top() == '[' && c == ']') st.pop();
-                else st.push(c);
-            }
-            else st.push(c);
+            else if(st.empty()) return false;
+            else if(c == ')' && st.top() == '(') st.pop();
+            else if(c == '}' && st.top() == '{') st.pop();
+            else if(c == ']' && st.top() == '[') st.pop();
+            else return false;
         }
         return st.empty();
     }
